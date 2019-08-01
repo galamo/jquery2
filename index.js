43dbbe8635b45525b965248d3b494ff5138c26a2
noUser <noUser>
2019-08-01 21:17:42 +0300

    bb and sara

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
-//my code
-const pictures = ["item1", "item2", "item3"]
-// $(function(){
-// document is ready
-// })
+
+
 
 $(document).ready(function () {
 
-    const header = $("#header");
-    let index = 0;
-    header.on("click", function () {
-        if (index >= pictures.length) return;
-        const currentId = pictures[index];
-        $("#" + currentId).fadeIn()
-        index++;
+    //cloning an object
+    const picture = $("#enemy").clone()
+    picture.attr({ id: getId() })
+    picture.css({ display: "inline-block", position: "absolute", left: getRandomLocation(), top: getRandomLocation() })
 
-    })
+    const picture2 = $("#enemy").clone()
+    picture2.attr({ id: getId() })
+    picture2.css({ display: "inline-block", position: "absolute", left: getRandomLocation(), top: getRandomLocation() })
+
+    const picture3 = $("#enemy").clone()
+    picture3.attr({ id: getId() })
+    picture3.css({ display: "inline-block", position: "absolute", left: getRandomLocation(), top: getRandomLocation() })
+
+    const player = $("#enemy").clone()
+    player.attr({ id: getId() })
+    player.css({ display: "inline-block", position: "absolute", left: getRandomLocation(), top: getRandomLocation() })
+    player.children().attr({ src: "./images/bb.jpg" })
+    player.draggable();
+
+    $("#board").append(picture)
+    $("#board").append(picture2)
+    $("#board").append(picture3)
+    $("#board").append(player)
 
 
-    $("button").on("click", function () {
-        $(".card").hide()
-    })
 
 
 
 })
+
+function getRandomLocation() {
+    return Math.round(Math.random() * 777)
+}
+// Build function taht generate enemies
+function getRandomNumber() {
+    Date().now
+}
+
+function getId() {
+    return Date.now()
+}
 
 
