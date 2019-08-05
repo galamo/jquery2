3481fd8fd30c9e58f98c7d48aa5d1151ed966c49
noUser <noUser>
2019-08-05 21:21:01 +0300

    game_done

--- models/BoardItem.js
+++ models/BoardItem.js
@@ -1,13 +1,22 @@
 //AKA function constructor
 // class for creating instances
 class BoardItem {
+    //image = { isUrl:true, path:"https://image.com/bb.jpg" }
+    //image = { isUrl:false, path:"ynet.png" }
 
     constructor(position, image, id) {
         this.image = image;
         this.domElement = $("#enemy").clone();
         const { top, left } = position;
         this.domElement.css({ position: "absolute", display: "inline-block", top, left })
+        this.domElement.attr({ id });
+        const { isUrl, path } = image
+        this.domElement.children().attr({ src: isUrl ? path : `./images/${path}` })
+        this.id = id
+    }
 
+    drawMe() {
+        $("#board").append(this.domElement)
     }
 
 
